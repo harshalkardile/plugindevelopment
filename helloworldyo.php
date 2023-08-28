127cc1bfeb8f0b97e99fba9b323bfd5daaab0f85
harshal <kardileharshal567@gmail.com>
2023-08-28 12:48:43 +0530

    initial commit

---
+++ helloworldyo.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hello World, Yo
+Description: My first wordpress plugin
+Version: 1.0
+Author: Harshal k
+Author URI: https://automattic.com/wordpress-plugins/
+License: GPLv2 or later
+*/
+
+echo "<div class='notice'>helo</div>";
